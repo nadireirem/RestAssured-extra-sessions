ebb4ea6c7f38657ed43f10b507a3917bc33edfe9
Mehmet <mhmtsytrk@gmail.com>
2022-08-08 18:51:52 -0400

    day01 oh

--- src/test/java/com/cydeo/tests/officehours/day1/APITask1.java
+++ src/test/java/com/cydeo/tests/officehours/day1/APITask1.java
@@ -2,6 +2,7 @@ package com.cydeo.tests.officehours.day1;
 
 import com.cydeo.utils.TypiCodeTestBase;
 import io.restassured.http.ContentType;
+import io.restassured.internal.common.assertion.Assertion;
 import io.restassured.response.Response;
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.Test;
@@ -34,5 +35,37 @@ public class APITask1 extends TypiCodeTestBase {
         assertEquals("application/json; charset=utf-8",response.contentType());
         assertEquals("application/json; charset=utf-8",response.getHeader("Content-Type"));
 
+    }
+
+
+    @Test
+    public void task2() {
+        //- Given accept type is Json
+        //- Path param "id" value is 1
+        //- When user sends request to  https://jsonplaceholder.typicode.com/posts/{id}
+
+        Response response = given().accept(ContentType.JSON).pathParam("id", 1)
+                .when().get("posts/{id}");
+
+        //- Then status code is 200
+         assertEquals(200,response.getStatusCode());
+
+        //-And json body contains "repellat provident"
+        assertTrue(response.asString().contains("repellat provident"));
+        //- And header Content - Type is Json
+        assertEquals("application/json; charset=utf-8",response.getContentType());
+
+
+        // How can we verify Date ?
+        System.out.println(response.getHeader("Date"));
+
+        assertTrue(response.getHeaders().hasHeaderWithName("Date"));
+
+        //- And header "X-Powered-By" value is "Express"
+
+        //- And header "X-Ratelimit-Limit" value is 1000
+        //- And header "Age" value is more than 100
+        //
+        //- And header "NEL" value contains "success_fraction"
     }
 }
