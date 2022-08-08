4e119615cc33f5f6696434291dbb355e92eeac9d
Mehmet <mhmtsytrk@gmail.com>
2022-08-08 18:07:25 -0400

    day01 oh

---
+++ src/test/java/com/cydeo/tests/officehours/day1/APITask1.java
@@ -0,0 +1,20 @@
+package com.cydeo.tests.officehours.day1;
+
+import org.junit.jupiter.api.Test;
+
+public class APITask1 {
+
+
+    @Test
+    public void task1() {
+    //Q1:
+        //- Given accept type is Json
+        //- When user sends request to https://jsonplaceholder.typicode.com/posts
+        //
+        //-Then print response body
+        //
+        //- And status code is 200
+        //- And Content - Type is Json
+
+    }
+}
