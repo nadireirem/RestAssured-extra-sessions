bcb44588b4d23b173cceeaba250e30cca7375c43
Mehmet <mhmtsytrk@gmail.com>
2022-08-08 18:24:43 -0400

    day01 oh

--- src/test/java/com/cydeo/tests/officehours/day1/APITask1.java
+++ src/test/java/com/cydeo/tests/officehours/day1/APITask1.java
@@ -1,20 +1,38 @@
 package com.cydeo.tests.officehours.day1;
 
+import com.cydeo.utils.TypiCodeTestBase;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.Test;
 
-public class APITask1 {
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class APITask1 extends TypiCodeTestBase {
 
 
     @Test
     public void task1() {
-    //Q1:
+
         //- Given accept type is Json
         //- When user sends request to https://jsonplaceholder.typicode.com/posts
-        //
+        Response response = given().log().uri().accept(ContentType.JSON)
+                .when().get("/posts");
+
         //-Then print response body
-        //
+        // it prints response body
+        response.prettyPrint();
+
         //- And status code is 200
+        assertEquals(200,response.statusCode());
+        assertEquals(200,response.getStatusCode());
+        assertEquals(HttpStatus.SC_OK,response.statusCode());
+
         //- And Content - Type is Json
+        assertEquals("application/json; charset=utf-8",response.getContentType());
+        assertEquals("application/json; charset=utf-8",response.contentType());
+        assertEquals("application/json; charset=utf-8",response.getHeader("Content-Type"));
 
     }
 }
