53b1542a4320b1db6bb8aa8e9e98a3a853f0ec28
Mehmet <mhmtsytrk@gmail.com>
2022-08-23 17:58:05 -0400

    day05 oh

---
+++ src/test/java/com/cydeo/tests/officehours/day5/APITasks4.java
@@ -0,0 +1,31 @@
+package com.cydeo.tests.officehours.day5;
+
+import com.cydeo.utils.HrApiTestBase;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
+public class APITasks4 extends HrApiTestBase {
+
+    //1) POST a region then do validations. Please use Map or POJO class, or JsonPath
+    ///**
+    // * given accept is json
+    // * and content type is json
+    // * When I send post request to "/regions/"
+    // * With json:
+    // * {
+    // *     "region_id":100,
+    // *     "region_name":"Test Region"
+    // * }
+    // * Then status code is 201
+    // * content type is json
+    // * region_id is 100
+    // * region_name is Test Region
+    // */
+
+
+    @Test
+    public void task1() {
+
+    }
+}
