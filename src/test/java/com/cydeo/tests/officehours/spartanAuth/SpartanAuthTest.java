e53e67c95e97d045b7b577dd6f90e9c9c3144044
Mehmet <mhmtsytrk@gmail.com>
2022-08-30 18:05:33 -0400

    day07 oh

--- src/test/java/com/cydeo/tests/officehours/spartanAuth/SpartanAuthTest.java
+++ src/test/java/com/cydeo/tests/officehours/spartanAuth/SpartanAuthTest.java
@@ -1,6 +1,27 @@
 package com.cydeo.tests.officehours.spartanAuth;
 
 import com.cydeo.utils.SpartanSecureTestBase;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
 public class SpartanAuthTest  extends SpartanSecureTestBase {
+
+    @Test
+    public void testPublicUser() {
+        given().accept(ContentType.JSON)
+                .get("/spartans").prettyPeek().
+                then().log().ifValidationFails()
+                .statusCode(401);
+    }
+
+    @Test
+    public void testAuthenticatedUser() {
+        given().accept(ContentType.JSON)
+                .auth().basic("user","user")
+                .get("/spartans").prettyPeek()
+                .then().log().ifValidationFails()
+                .statusCode(200);
+    }
 }
