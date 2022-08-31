99eb7fba1a0f296f4cc999fdbbb0a242ce3201d4
Mehmet <mhmtsytrk@gmail.com>
2022-08-31 17:56:47 -0400

    newsAPI

---
+++ src/test/java/com/cydeo/tests/officehours/newsAPI/NewsAPI.java
@@ -0,0 +1,12 @@
+package com.cydeo.tests.officehours.newsAPI;
+
+import com.cydeo.utils.NewsAPITestBase;
+import org.junit.jupiter.api.Test;
+
+public class NewsAPI extends NewsAPITestBase {
+
+    @Test
+    public void task1() {
+
+    }
+}
