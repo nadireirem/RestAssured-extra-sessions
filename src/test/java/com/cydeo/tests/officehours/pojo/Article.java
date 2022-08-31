94fd6b574f904f21ccc57d109c858fa6b89ff9fc
Mehmet <mhmtsytrk@gmail.com>
2022-08-31 19:02:32 -0400

    newsAPI

--- src/test/java/com/cydeo/tests/officehours/pojo/Article.java
+++ src/test/java/com/cydeo/tests/officehours/pojo/Article.java
@@ -1,12 +1,11 @@
 package com.cydeo.tests.officehours.pojo;
 
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class Article{
-    @JsonProperty
+
     SourcePOJO source;
     String author;
     String title;
